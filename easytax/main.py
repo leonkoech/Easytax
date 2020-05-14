a95e74d47d0be9fee8111a74943fd33e280dacf6
leonkoech <leonkipkip@gmail.com>
2020-05-14 10:57:16 +0300

    file nil returns

--- easytax/main.py
+++ easytax/main.py
@@ -20,7 +20,7 @@ def openkra():
 
     driver = webdriver.Chrome(options=options, executable_path='/usr/local/bin/chromedriver')
     driver.get('https://itax.kra.go.ke/')
-    time.sleep(5)#sleep for 3 seconds after page has loaded
+    time.sleep(3)#sleep for 3 seconds after page has loaded
     inputpin = driver.find_element_by_xpath('//*[@id="logid"]')
     inputpin.send_keys(secrets.pin)
     time.sleep(3)#sleep for 10 seconds to see input
@@ -37,6 +37,22 @@ def openkra():
     time.sleep(8)
     action = ActionChains(driver)
     action.move_to_element(driver.find_element_by_xpath('//*[@id="ddtopmenubar"]/ul/li[3]/a')).perform()
+    # move to file nil returns
+    filenil = driver.find_element_by_xpath('//*[@id="Returns"]/li[4]/a')
+    # hover over the item
+    action.move_to_element(filenil).perform()
+    # now click on the item
+    filenil.click()
+    time.sleep(3)
+    # find the  options
+    driver.find_element_by_xpath('//*[@id="regType"]').click()
+    # and hover over resident
+    # click on it
+    driver.find_element_by_xpath('//*[@id="regType"]/option[2]').click()
+    time.sleep(2)
+    # click the next button
+    driver.find_element_by_xpath('//*[@id="btnSubmit"]')
+    time.sleep(10)
     
     driver.quit()
 def downloadimage(driver,link):
@@ -51,10 +67,11 @@ def readimage():
             text=text.replace(t,' ')
             text=text.split()
             ans=int(text[0])+int(text[1])
+            return ans
         elif t=='-':
             text=text.replace(t,' ')
             text=text.split()
             ans=int(text[0])-int(text[1])
-    return ans
+            return ans
 if __name__=='__main__':
     openkra()
